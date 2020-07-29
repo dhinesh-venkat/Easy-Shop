d28b494a64ba302e113a97748f05c343c6db3963
Dhinesh <dhinesh5124@gmail.com>
2020-07-29 13:44:34 +0530

    first commit

--- lib/services/group_service.dart
+++ lib/services/group_service.dart
@@ -9,38 +9,22 @@ class GroupService {
 
   Future<APIResponse<List<Group>>> getGroupList() {
     return http.get(url + '/api//group?&pagenumber=0&pagesize=10').then((data) {
-      if(data.statusCode == 200) {
+      if (data.statusCode == 200) {
         final jsonData = json.decode(data.body);
         final groups = <Group>[];
-        for(var item in jsonData) {
-          final group = Group(id: item['ID'], value: item['VALUE'],imageUrl: "https://cdn.pixabay.com/photo/2015/09/02/12/25/basket-918416_960_720.jpg");
-        groups.add(group);
+        for (var item in jsonData) {
+          final group = Group(
+              id: item['ID'],
+              value: item['VALUE'],
+              imageUrl:
+                  "https://cdn.pixabay.com/photo/2015/09/02/12/25/basket-918416_960_720.jpg");
+          groups.add(group);
         }
         return APIResponse<List<Group>>(data: groups);
       }
-      return APIResponse<List<Group>>(error: true, errorMessage: 'An error occured');
-    }).catchError((_) => APIResponse<List<Group>>(error: true, errorMessage: 'An error occured'));;
-    // return [
-    //   Group(
-    //       id: "1",
-    //       value: "Oil",
-    //       imageUrl:
-    //           "https://cdn.pixabay.com/photo/2015/09/02/12/25/basket-918416_960_720.jpg"),
-    //   Group(
-    //       id: "2",
-    //       value: "Beauty",
-    //       imageUrl:
-    //           "https://cdn.pixabay.com/photo/2015/09/02/12/25/basket-918416_960_720.jpg"),
-    //   Group(
-    //       id: "3",
-    //       value: "Vitamin",
-    //       imageUrl:
-    //           "https://cdn.pixabay.com/photo/2015/09/02/12/25/basket-918416_960_720.jpg"),
-    //   Group(
-    //       id: "4",
-    //       value: "Electronics",
-    //       imageUrl:
-    //           "https://cdn.pixabay.com/photo/2015/09/02/12/25/basket-918416_960_720.jpg")
-    // ];
+      return APIResponse<List<Group>>(
+          error: true, errorMessage: 'An error occured');
+    }).catchError((_) => APIResponse<List<Group>>(
+        error: true, errorMessage: 'An error occured'));
   }
 }
